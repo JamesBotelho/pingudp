54f757e80a7013563db0df621aac48adae5ce9de
James <james_oliveira@live.com>
2017-11-23 18:24:20 -0200

    Cliente Implementado (adicionado comentários)

--- src/Cliente/PingClient.java
+++ src/Cliente/PingClient.java
@@ -29,26 +29,42 @@ public class PingClient {
         int porta = Integer.parseInt(args[1]);*/
         InetAddress endereco = InetAddress.getByName("127.0.0.1");
         int porta = 8080;
-        int n_sequencia = 0;
-        String mensagem;
-        byte[] buffer = new byte[1024];
+        int n_sequencia = 0; //Número de iteradas que é utilizado na mensagem
+        String mensagem;    //Mensagem que será enviada
+        byte[] buffer = new byte[1024]; //buffer que será usado no envio da mensagem para o servidor
         while(n_sequencia < 10){
             Date tempo = new Date();
             long tempo_ms = tempo.getTime();
             mensagem = "PING " + String.valueOf(n_sequencia) + " " + tempo_ms + "\n";
             buffer = mensagem.getBytes();
+            
+            //Prepara o pacote para o envio
             DatagramPacket pkg = new DatagramPacket(buffer, buffer.length, endereco, porta);
+            
+            //Abre a conexão
             DatagramSocket ds = new DatagramSocket();
+            
+            //Envia o pacote para o destino
             ds.send(pkg);
             try{
+                
+                //Define o tempo de espera pelo pacote
                 ds.setSoTimeout(TIMEOUT);
+                
+                //Recebe o pacote enviado pelo servidor
                 ds.receive(pkg);
                 tempo = new Date();
+                
+                //Efetua o calculo do Ping
                 tempo_ms = tempo.getTime() - tempo_ms;
+                
+                //Exibe a mensagem, caso o pacote seja retornado a tempo
                 System.out.print("PING " + String.valueOf(n_sequencia) + "\t" + tempo_ms + " ms\n");
             }catch(IOException e){
+                //Caso o pacote não seja retornado no tempo determinado, exibe a mensagem abaixo
                 System.out.print("Pacote " + String.valueOf(n_sequencia) + " perdido\n");
             }
+            //Incrementa o número de sequência
             n_sequencia++;
         }
     }
